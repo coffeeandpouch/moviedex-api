2961fc3afa7666faf3fdb800fd17728c111886cc
coffeeandpouch <sameinsidler@me.com>
2020-12-17 11:38:49 -0600

    commits edited server.js

--- server.js
+++ server.js
@@ -1,59 +1,51 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const helmet = require("helmet");
 const cors = require("cors");
+const helmet = require("helmet");
 const MOVIES = require("./movies-data-small.json");
-
-console.log(process.env.API_TOKEN);
 
 const app = express();
 
 app.use(morgan("dev"));
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
 
-app.use(validateBearerToken);
-
-function validateBearerToken(req, res, next) {
+app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
-    return res.status(401).json({ error: "Unauthorized Request" });
+    return res.status(401).json({ error: "Unauthorized request" });
   }
   next();
-}
+});
 
-app.get("/movie", (req, res) => {
-  // let response = MOVIES;
+app.get("/movie", function handleGetMovie(req, res) {
+  let response = MOVIES;
 
-  const validGenres = [...new Set(MOVIES.map((m) => m.genre))];
-
-  let results = MOVIES;
-
-  if (req.query.genre && !validGenres.includes(req.query.genre)) {
-    res.status(400).json({ error: "Invalid Genre" });
-  }
-
-  if (req.query.genre && validGenres.includes(req.query.genre)) {
-    results = results.filter((m) => m.genre === req.query.genre);
-  }
-
-  if (req.query.country) {
-    results = results.filter((m) => m.country === req.query.country);
-  }
-
-  if (req.query.avg_vote) {
-    results = results.filter(
-      (m) => Number(m.avg_vote) >= Number(req.query.avg_vote)
+  if (req.query.genre) {
+    response = response.filter((movie) =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
     );
   }
 
-  res.json(results);
+  if (req.query.country) {
+    response = response.filter((movie) =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(
+      (movie) => Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
+
+  res.json(response);
 });
 
 const PORT = 8000;
 
 app.listen(PORT, () => {
-  console.log(`server listening at http://localhost:${PORT}`);
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
